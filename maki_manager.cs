fce5edd3cd436b19d6d3460eaf5bfe63b79477dc
Mofuryu <36126578+krotoselqui@users.noreply.github.com>
2022-11-22 15:45:29 +0900

    pickup取得まで

--- maki_manager.cs
+++ maki_manager.cs
@@ -1,15 +1,40 @@
-﻿
+
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
+[UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)] 
 public class maki_manager : UdonSharpBehaviour
 {
-    [SerializeField] private AudioClip[] collisionSounds;
+    [Header("音声系統")]
+    [SerializeField] private AudioClip[] burnSounds;
+    [SerializeField] private AudioSource[] auSrc;
+
+    [Header("火元")]
+    [SerializeField] private Transform fireTransform;
 
     void Start()
     {
+        //pickupコンポーネントを取得
+        pickup = (VRC_Pickup)this.GetComponent(typeof(VRC_Pickup));
     }
+
+    //privateでもよさそう？
+    public void Update()
+    {
+        if (pickup) pickup.pickupable = true;
+    }
+
+    public override void OnDrop()
+    {
+        if (!pickup) pickup = (VRC_Pickup)this.GetComponent(typeof(VRC_Pickup));
+        if (pickup)  pickup.PlayHaptics();
+        
+        //audio.PlayOneShot(burnSounds[Random.Range(0, burnSounds.Length)]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(BurnFireWood));
+     
+    }
+
 
 }
