a794c8ba8a13d2f44649119e4337cd523c4c1d57
Krotoselqui <yusei.ohmasa@sinei-design.co.jp>
2023-03-10 10:50:44 +0900

    Update maki_manager.cs

--- bonfire firewood/Scripts/maki_manager.cs
+++ bonfire firewood/Scripts/maki_manager.cs
@@ -1,127 +1,121 @@
-
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
-public class maki_manager : UdonSharpBehaviour
+namespace Mofuryu
 {
-    [Header("燃える音(この中で1つランダム)")]
-    [SerializeField] private AudioClip[] burnSounds;
-
-    [Header("音の発生源")]
-    [SerializeField] private AudioSource audioSrc;
-
-    [Header("火元")]
-    [SerializeField] private Transform[] fireTransform;
-    private Vector3[] firePosition;
-
-    [Header("火元判定距離(m)")]
-    [SerializeField] float distBurnThreshold = 0.4f;
-
-    [Header("薪燃焼エフェクト(すべて再生)")]
-    [SerializeField] private ParticleSystem[] burnParticle;
-
-    [FieldChangeCallback(nameof(vrcPick))]
-    private VRC_Pickup _vrcPick;
-    private VRC_Pickup vrcPick => _vrcPick ? _vrcPick : (_vrcPick = (VRC_Pickup)GetComponent(typeof(VRC_Pickup)));
-
-    private Vector3 initPos;
-    private Quaternion initRot;
-
-    [Header("燃え始めの瞬間～リスポーンまでの時間(sec)")]
-    [SerializeField] private float respawnTime = 2.0f;
-
-    private float currentTime = 0f;
-    private bool isBurning = false;
-    private int fireIndex = 0;
-
-    public void Start()
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
+    public class maki_manager : UdonSharpBehaviour
     {
-        vrcPick.InteractionText = "FireWood";
-        vrcPick.DisallowTheft = true;
+        //SERIALIZED
+        [Header("燃える音(この中で1つランダム)")]
+        [SerializeField] private AudioClip[] burnSound;
 
-        initPos = this.gameObject.transform.position;
-        initRot = this.gameObject.transform.rotation;
+        [Header("音発生源")]
+        [SerializeField] private AudioSource audioSrc;
 
-        if (fireTransform != null)
+        [Header("火元")]
+        [SerializeField] private Transform[] fireTransform;
+
+        [Header("火元判定距離(m)")]
+        [SerializeField] float distBurnThreshold = 0.4f;
+
+        [Header("薪燃焼エフェクト(すべて再生)")]
+        [SerializeField] private ParticleSystem[] burnParticle;
+        
+        [Header("燃え始めの瞬間～リスポーンまでの時間(sec)")]
+        [SerializeField] private float respawnTime = 2.0f;
+
+        //INTERNAL
+        private Vector3[] firePosition;
+        private Vector3 initPos;
+        private Quaternion initRot;
+
+        private float currentTime = 0f;
+        private bool isBurning = false;
+        
+        //VRCPICKUP
+        [FieldChangeCallback(nameof(vrcPick))]
+        private VRC_Pickup _vrcPick;
+        private VRC_Pickup vrcPick => _vrcPick ? _vrcPick : (_vrcPick = (VRC_Pickup)GetComponent(typeof(VRC_Pickup)));
+
+        public void Start()
         {
-            firePosition = new Vector3[fireTransform.Length];
-            for (int i = 0; i < fireTransform.Length; i++)
-            {
+            SetTextPickup("FireWood");
+            SetTheftablePickup(false);
+
+            initPos = this.gameObject.transform.position;
+            initRot = this.gameObject.transform.rotation;
+
+            firePosition = new Vector3[fireTransform?.Length ?? 0];
+
+            for(int i = 0; i < fireTransform?.Length ?? 0; i++){
                 firePosition[i] = fireTransform[i].position;
             }
+
+            isBurning = false;
         }
 
-        isBurning = false;
-    }
-
-    public void Update()
-    {
-        if (isBurning)
+        public void Update()
         {
-            DisablePickUp();
-            if (currentTime < 0)
+            if (isBurning)
             {
-                EnablePickUp();
-                isBurning = false;
-                if (Networking.IsOwner(Networking.LocalPlayer, this.gameObject))
-                    this.transform.SetPositionAndRotation(initPos,initRot);
+                DisablePickUp();
+                if (currentTime < 0)
+                {
+                    isBurning = false;
+                    if (Networking.IsOwner(Networking.LocalPlayer, this.gameObject))
+                        this.transform.SetPositionAndRotation(initPos,initRot);
+                    EnablePickUp();
+                }
+                else
+                {
+                    currentTime -= Time.deltaTime;
+                }
             }
             else
             {
-                currentTime -= Time.deltaTime;
+                EnablePickUp();
             }
         }
-        else
-        {
-            EnablePickUp();
-        }
-    }
 
-    public override void OnDrop()
-    {
-        PlayHaptics();
-
-        float min_dist = float.MaxValue;
-        if (fireTransform != null)
+        public override void OnDrop()
         {
-            for(int i = 0; i < firePosition.Length; i++)
+            PlayHaptics();
+
+            float min_dist = float.MaxValue;
+            foreach (Vector3 position in firePosition ?? new Vector3[0])
             {
-                float dist = Vector3.Distance(this.gameObject.transform.position, firePosition[i]);
-                if (min_dist > dist)
-                {
-                    min_dist = dist;
-                    fireIndex = i;
-                }
+                float dist = Vector3.Distance(transform.position, position);
+                if (dist < min_dist) min_dist = dist;
+            }
+            
+            if (min_dist < distBurnThreshold)
+            {
+                currentTime = respawnTime;
+                isBurning = true;
+                DisablePickUp();
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(Burn));
             }
         }
-        
-        if (min_dist < distBurnThreshold)
+
+        public void Burn()
         {
-            currentTime = respawnTime;
-            isBurning = true;
-            DisablePickUp();
-
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(BurnFireWood));
+            //Sounds            
+            if (audioSrc && !audioSrc.isPlaying)
+                audioSrc.PlayOneShot(burnSound[Random.Range(0, burnSound.Length)]);
+            
+            //Particles
+            foreach (ParticleSystem burn_pt in burnParticle) if (burn_pt != null) burn_pt.Play();
         }
-    }
 
-    public void BurnFireWood()
-    {
-        //Sounds            
-        if (audioSrc && !audioSrc.isPlaying)
-        {
-            audioSrc.transform.position = fireTransform[fireIndex].position;
-            audioSrc.PlayOneShot(burnSounds[Random.Range(0, burnSounds.Length)]);
-        }
-        
-        //Particles
-        foreach (ParticleSystem burn_pt in burnParticle) if (burn_pt != null) burn_pt.Play();
-    }
+        //DETACHABLE WITH VRCPICKUP DECLARES
+        private void DisablePickUp() => vrcPick.pickupable = false;
+        private void EnablePickUp() => vrcPick.pickupable = true;
+        private void PlayHaptics() => vrcPick.PlayHaptics();
+        private void SetTextPickup(string st) => vrcPick.InteractionText = st;
+        private void SetTheftablePickup(bool b) => vrcPick.DisallowTheft = b;
 
-    private void DisablePickUp() => vrcPick.pickupable = false;
-    private void EnablePickUp() => vrcPick.pickupable = true;
-    private void PlayHaptics() => vrcPick.PlayHaptics();
+    }
 }
